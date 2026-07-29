71773a260c3dcd840c9231fd3c46e0dd37e7165a
Mateen-Q <qureshimateen6265@gmail.com>
2026-07-29 23:15:07 +0530

    Created vendor repository

---
+++ src/main/java/com/stampd/router/repository/VendorRepository.java
@@ -0,0 +1,9 @@
+package com.stampd.router.repository;
+
+import com.stampd.router.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository {
+}
