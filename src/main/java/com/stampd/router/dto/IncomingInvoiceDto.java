bcfeecc1d2b4ab2a8abe97b9d3f60a519a017540
Mateen-Q <qureshimateen6265@gmail.com>
2026-08-03 11:37:07 +0530

    added getters and setters for Incoming Invoice Dto file

--- src/main/java/com/stampd/router/dto/IncomingInvoiceDto.java
+++ src/main/java/com/stampd/router/dto/IncomingInvoiceDto.java
@@ -1,10 +1,32 @@
 package com.stampd.router.dto;
 
-import lombok.Data;
 
-@Data
 public class IncomingInvoiceDto {
     private String poNumber;
     private Double totalAmount;
     private String vendorName;
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
 }
